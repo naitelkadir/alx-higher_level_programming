2027068d609c5a168efa79a7c3b9ff5da10b84b7
naitelkadir <naitelkadir@gmail.com>
2023-05-09 20:56:31 +0000

    test

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-for i in range(122, 96, -1):
-    if i % 2 == 0:
-        print(chr(i), end="")
-    else:
-        print(chr(i - 32), end="")
+for i in "zYxWvUtSrQpOnMlKjIhGfEdCbA":
+    print("{}".format(i), end="")
 
