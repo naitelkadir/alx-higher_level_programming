0c65d51c65b24ef8e7c2abbc035c57a41fcda75c
naitelkadir <naitelkadir@gmail.com>
2023-05-09 14:25:13 +0000

    test

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+for i in range(0, 90):
+    if i / 10 > i % 10:
+        continue
+    else:
+        if (i != 89):
+            print("{:02}".format(i), end=", ")
+        else:
+            print("{}".format(i))
