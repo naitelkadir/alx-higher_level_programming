24fab0b6df7a54a3a913f6bacba6879af99998ad
naitelkadir <naitelkadir@gmail.com>
2023-05-17 13:35:31 +0000

    test

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -4,7 +4,7 @@ def weight_average(my_list=[]):
         return (0)
     s = 0
     res = 0
-    for i in range(len(my_list)):
-        res += my_list[i][0] * my_list[i][1]
-        s +=  my_list[i][1]
+    for tup in my_list:
+        res += tup[0] * tup[1]
+        s += tup[1]
     return (res / s) 
