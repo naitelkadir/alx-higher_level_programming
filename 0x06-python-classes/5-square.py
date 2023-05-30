d17e60b30d566bf179b0fa7d8db41a188c00e67c
naitelkadir <naitelkadir@gmail.com>
2023-05-30 14:31:30 +0000

    update

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -31,4 +31,4 @@ class Square:
             for i in range(self.__size):
                 for j in range(self.__size):
                     print("#", end="")
-                print("\n")
+                print("")
