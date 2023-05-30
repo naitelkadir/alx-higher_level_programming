009679e565084dbee58ae0cb071022d0d44aad16
naitelkadir <naitelkadir@gmail.com>
2023-05-30 11:32:53 +0000

    add task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Define a class square."""
+class Square:
+    """Represent a  square"""
+    def __init__(self, size):
+        """initialize new sqyare.
+        Args:
+        size(int): the size of the new square.
+        """
+        self.__size = size
