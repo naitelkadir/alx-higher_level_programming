a289b262eeb5184250998dbe4b36ba94bb9a1733
naitelkadir <naitelkadir@gmail.com>
2023-05-30 15:08:49 +0000

    test

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""Define a new square"""
+class Square:
+    """Represent a square"""
+    def __init__(self, size=0, position=(0, 0)):
+        """initialize a square
+        Args:
+             size of the square
+        """
+        self.size = size
+        self.position = position
+    @property
+    def size(self):
+        """ proprety to retrive the size"""
+        return (self.__size)
+    @size.setter
+    def size(self, value):
+        """proprety to set size to a value"""
+        self.__size = value
+        if not type(value) is int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+    @property
+    def position(self):
+        """ proprety to retrive the position"""
+        return (self.__position)
+    @size.setter
+    def position(self, value):
+        """proprety to set position to a value"""
+        self.__position = value
+        if len(value) != 2 or not type(value[0]) is int or not type(value[1]) is int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+    def area(self):
+        """return the current area"""
+        return (self.__size ** 2)
+    def my_print(self):
+        """method to print the square"""
+        if (self.size == 0):
+            print("")
+        else:
+            for i in range(self.__size):
+                for j in range(self.__position[0]):
+                    print(" ", end="")
+                for k in range(self.__size):
+                    print("#", end="")
+                print("")
