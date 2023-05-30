61999aaef43799ce64cf3e84f7270449ec2e48e5
naitelkadir <naitelkadir@gmail.com>
2023-05-30 12:04:41 +0000

    add task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Reprsent a square"""
+class Square:
+    def __init__(self, size=0):
+        """Intialize a new Square
+        Args:
+            size(int): the size of the new square
+        """
+        self.__size = size
+        if not type(size) is int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
