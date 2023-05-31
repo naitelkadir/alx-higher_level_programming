ee5d4c1a181dc799bbbf105c0a10cb51f7e93f85
naitelkadir <naitelkadir@gmail.com>
2023-05-31 10:26:41 +0000

    test

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""Define a new square"""
+class Square:
+    """Represent a square"""
+    def __init__(self, size=0):
+        """initialize a square
+        Args:
+             size of the square
+        """
+        self.size = size
+    @property
+    def size(self):
+        """ proprety to retrive the size"""
+        return (self.__size)
+    @size.setter
+    def size(self, value):
+        """proprety to set size to a value"""
+        self.__size = value
+        if not type(value) is int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+    def area(self):
+        """return the current area"""
+        return (self.__size ** 2)
+    def __eq__(self, other):
+        """ define == comaparator"""
+        return self.area() == other.area()
+    def __df__(self, other):
+        """define != comaparator"""
+        return self.area() != self.other
+    def __inf__(self, other):
+        """define < comparator"""
+        return self.area() < other.area()
+    def __infeq__(self, other):
+        """ define <= comparator"""
+        return self.area() <= other.area()
+    def __sup__(self, other):
+        """define > comparator"""
+        return self.area() > other.area()
+    def __supeq__(self, other):
+        """define >= comparator"""
+        return self.area() >= other.area()
