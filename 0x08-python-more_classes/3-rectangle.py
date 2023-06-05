756a87bb152290c8fc4518500dd76afd773e2e29
naitelkadir <naitelkadir@gmail.com>
2023-06-05 12:13:03 +0000

    test

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -43,7 +43,7 @@ class Rectangle:
     def __str__(self):
         """represent the rectangle"""
         if self.__width == 0 or self.__height == 0:
-            return ()
+            return ("")
         new = []
         for i in range(self.__height):
             for j in range(self.__width):
