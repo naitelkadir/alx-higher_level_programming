8fbe7f8922cb74baea1977a5e4fc747f642778be
naitelkadir <naitelkadir@gmail.com>
2023-06-12 19:09:27 +0000

    add task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""define a new class"""
+class BaseGeometry:
+    """define a basegeometry class"""
+    def area(self):
+        """a function that raise an exception"""
+        raise Exception("area() is not implemented")
