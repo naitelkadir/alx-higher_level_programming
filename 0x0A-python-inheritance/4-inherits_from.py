7af642446120b677677a3f0da1292e384105522a
naitelkadir <naitelkadir@gmail.com>
2023-06-12 15:13:16 +0000

    test

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -2,7 +2,6 @@
 """define a function to verify the instance of an object"""
 def inherits_from(obj, a_class):
     """return True if the object is exactly instance of the sepecified class"""
-    if isinstance(obj, a_class):
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
         return (True)
-    else:
-        return (False)
+    return (False)
