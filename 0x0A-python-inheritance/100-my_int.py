4ad35afc943316e7db2a2368a2c69c20039df334
naitelkadir <naitelkadir@gmail.com>
2023-06-12 23:04:53 +0000

    add task 100

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""define a class Myint inherits from int"""
+class MyInt(int):
+    """inverts == and != int operators"""
+    def __eq__(self, value):
+        """invert to == to !="""
+        return self.real != value
+    def __ne__(self, value):
+        """inverts != to =="""
+        return self.real == value
