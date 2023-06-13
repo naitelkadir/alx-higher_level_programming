eb788a8e6bbfad24c24c8682bc14ac81de329afc
naitelkadir <naitelkadir@gmail.com>
2023-06-13 20:42:28 +0000

    test

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Define a new student class"""
+
+
+class Student:
+    """Represent the student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """initialize the student class.
+
+        Args:
+            first_name(str): the first name
+            last_name(str): the last name
+            age(int): the age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self,  attrs=None):
+        """return a dict representation of a class"""
+        if attrs == None:
+            return (self.__dict__)
+        d = {}
+        for x in attrs:
+            for y in (self.__dict__):
+                if x == y:
+                    d[x] = self.__dict__[y]
+        return (d)
