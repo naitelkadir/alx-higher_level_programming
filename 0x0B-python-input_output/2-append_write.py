972439a0818984c100667aa8742ce8b1c3adb861
naitelkadir <naitelkadir@gmail.com>
2023-06-13 11:44:41 +0000

    test

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ define a function that appends a string to a file"""
 
+
 def append_write(filename="", text=""):
     """use with statement to append a string to a file"""
     with open(filename, 'a', encoding="utf-8") as f:
