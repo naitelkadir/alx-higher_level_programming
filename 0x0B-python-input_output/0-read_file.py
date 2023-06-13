fadfe45da1f7056d02ca32d9d79372d14c856612
naitelkadir <naitelkadir@gmail.com>
2023-06-13 10:30:44 +0000

    test

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 """ define a function that reads a text file and prints it to stdout"""
 
+
 def read_file(filename=""):
-    """use with statement to read the file.
-    
-    Args:
-        filename(string): name of the file to read
-    """
+    """use with statement to read the file."""
     with open(filename, encoding="utf-8") as f:
         data = f.read()
         print(data, end="")
