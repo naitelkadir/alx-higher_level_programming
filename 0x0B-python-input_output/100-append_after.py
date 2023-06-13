c207a416efbab32740b62b19914c15ca861548c3
naitelkadir <naitelkadir@gmail.com>
2023-06-13 23:36:39 +0000

    test

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""define a function that adds a line to a file"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """ inserts a line of text to a file, after each line containing a specific string"""
+    txt = ""
+    with open(filename) as fp:
+        for line in fp:
+            txt += line
+            if search_string in line:
+                txt += new_string
+    with open(filename, 'w') as f:
+        f.write(txt)
