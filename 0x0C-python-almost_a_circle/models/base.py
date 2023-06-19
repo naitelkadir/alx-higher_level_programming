b5d67cb9ad162c982e10f734628df56c2e5d8ad1
naitelkadir <naitelkadir@gmail.com>
2023-06-19 22:37:15 +0000

    test

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -29,11 +29,16 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """convert a list to json string"""
+        """Return the JSON serialization of a list of dicts.
+
+        Args:
+            list_dictionaries (list): A list of dictionaries.
+        """
         if list_dictionaries is None or list_dictionaries == []:
             return ("[]")
         else:
             return json.dumps(list_dictionaries)
+
     @classmethod
     def save_to_file(cls, list_objs):
         """write json string to a file"""
