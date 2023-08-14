fec4ea994b03944554e85af1ee1b1cb486a204fd
KHADIJA NAIT ELKADIR <111744649+naitelkadir@users.noreply.github.com>
2023-08-14 15:56:36 +0200

    Delete 0x0D-SQL_introduction directory

--- 0x0D-SQL_introduction/10-top_score.sql
+++
@@ -1,4 +0,0 @@
--- a script that lists all records of the table second_table
-SELECT 'score', 'name'
-FROM 'second_table'
-ORDER BY 'score' DESC;
--- 0x0D-SQL_introduction/2-remove_database.sql
+++
@@ -1,2 +0,0 @@
--- Write a script that deletes the database hbtn_0c_0 in your MySQL server.
-DROP DATABASE IF NOT EXISTS 'hbtn_0c_0';
